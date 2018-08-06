2e70a90227eaa254be610434937e2781c9b4526d
birozso <birozso@gmail.com>
2018-08-07 01:32:53 +0200

    moveToShell and Route methods

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchComponent from './SearchComponent'
 import Mains from './Mains'
+import { Route } from 'react-router-dom'
 
-class BooksApp extends React.Component {
+class App extends React.Component {
   state = {
     allBooks: []
 
@@ -20,12 +21,35 @@ class BooksApp extends React.Component {
 
   }
 
+  moveToShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf);
+    BooksAPI.getAll().then((books) => {
+      this.setState({ allBooks: books })
+
+    })
+  
+  }
+
   render() {
     return (
       <div className="app">
-        <Mains
-          ListBooks = {this.state.allBooks}
-        />
+
+      <Route exact path = "/" render = {() => (
+          <Mains
+              listBooks = {this.state.allBooks}
+              moveToShelf = {this.moveToShelf}
+          />
+        )} 
+      />
+
+      <Route path = "/search" render = {() => (
+          <SearchComponent 
+             moveToShelf = {this.moveToShelf}
+             listBooks = {this.state.allBooks}
+          />
+        )} 
+      />
+        
       </div>
 
     )
@@ -33,4 +57,4 @@ class BooksApp extends React.Component {
   }
 }
 
-export default BooksApp
+export default App
