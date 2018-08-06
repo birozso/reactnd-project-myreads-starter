38a362abde935eb47fd409f235b07528a95db8ff
birozso <birozso@gmail.com>
2018-08-07 01:28:43 +0200

    thumbnail conditional  added

--- src/BookComponent.js
+++ src/BookComponent.js
@@ -3,16 +3,20 @@ import React, { Component } from 'react';
 class BookComponent extends Component {
 
     render() {
+        let checkThumbnailExists = this.props.listBooks.imageLinks ? this.props.listBooks.imageLinks.thumbnail : '';
+
+
         return(
             <div className="book">
                 <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${this.props.listBooks.imageLinks.thumbnail}"` }}>
+                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${checkThumbnailExists}"` }}>
                     </div>
                     <div className="book-shelf-changer">
                         <select
                          onChange = {(e) => this.props.moveToShelf(
                             this.props.listBooks, e.target.value)
                             }
+
                             value = {this.props.belongsToShelf}
                         >
                             <option value="move" disabled>Move to...</option>
@@ -21,6 +25,7 @@ class BookComponent extends Component {
                             <option value="read">Read</option>
                             <option value="none">None</option>
                         </select>
+
                     </div>
                 </div>
                 <div className="book-title">{this.props.listBooks.title}</div>
